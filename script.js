bd3da26089d10f20259785780ff6acd13c59f6a3
Ron <aarontalua14@gmail.com>
2023-05-06 21:28:25 +0800

    Organized files, Started the project

---
+++ script.js
@@ -0,0 +1,23 @@
+// Initial library
+let myLibrary = [];
+
+// Book constructor
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = `${pages} pages`;
+    this.isRead = isRead;
+}
+
+// Create books then add to library
+const addBookToLibrary = function(title, author, pages, isRead) {
+    // Create new book using constructor
+    newBook = new Book(title, author, pages, isRead);
+
+    // Add it to library
+    myLibrary.push(newBook);
+
+    // Update displayed library
+
+}
+
